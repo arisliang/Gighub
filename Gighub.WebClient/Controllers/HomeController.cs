f719d97cdb3e794a6011efa200c9c6a69a501154
Liang Yi <liangyi2012@gmail.com>
2017-07-03 02:16:09 +0800

    improve look and feel of home page.

--- Gighub.WebClient/Controllers/HomeController.cs
+++ Gighub.WebClient/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Gighub.WebClient.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Gighub.WebClient.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre_)
+                .Where(g => g.Date > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public IActionResult About()
--- Gighub.WebClient/Models/ApplicationUser.cs
+++ Gighub.WebClient/Models/ApplicationUser.cs
@@ -9,5 +9,12 @@ namespace Gighub.WebClient.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Name
+        {
+            get
+            {
+                return UserName;
+            }
+        }
     }
 }
